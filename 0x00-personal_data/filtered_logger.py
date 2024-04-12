9802e9bc8ab9e3d3909f03d7643e22ecc92c4748
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-04-12 15:34:00 +0000

    filtered_logger.py

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""0. Regex-ing"""
+from typing import (List, Mapping, Tuple)
+import re
+
+
+def filter_datum(fields: List[str], redaction: str,
+                    message: str, separator: str) -> str:
+    """returns the log message obfuscated"""
+    for field in fields:
+        pattern = r'(?<=' + field + r'=)[^' + separator + ']+'
+        message = re.sub(pattern, redaction, message)
+    return message
