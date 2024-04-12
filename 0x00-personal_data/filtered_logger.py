575b01decc088efd7ad4a5628ee998d48b0f3f74
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-04-12 15:40:12 +0000

    filtered_logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,8 +4,9 @@ from typing import (List, Mapping, Tuple)
 import re
 
 
-def filter_datum(fields: List[str], redaction: str,
-                    message: str, separator: str) -> str:
+def filter_datum(
+        fields: List[str], redaction: str,
+        message: str, separator: str) -> str:
     """returns the log message obfuscated"""
     for field in fields:
         pattern = r'(?<=' + field + r'=)[^' + separator + ']+'
