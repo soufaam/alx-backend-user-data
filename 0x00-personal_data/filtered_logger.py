c78708985ad7b515203e45f72ef39cec1148889c
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-04-13 12:02:21 +0000

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -80,5 +80,24 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
         user=user,
         password=password,
         host=host,
+        port=3306,
         database=db_name
         )
+
+
+def main() -> None:
+    """ function that takes no arguments
+    and returns nothing"""
+    db = get_db()
+    logger = get_logger()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    for row in cursor:
+        formatted_msg = f'name={row[0]}; email={row[1]}; phone={row[2]};\
+ ssn={row[3]}; password={row[4]}; ip={row[5]}; last_login={row[6]}; user_agent={row[7]}'
+        logger.info(formatted_msg)
+    cursor.close()
+    db.close()
+
+if __name__ == "__main__":
+    main()
