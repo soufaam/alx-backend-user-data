f3bd0f2608513ff15d39c57dfabacaf5d9db1a8c
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-04-13 13:57:40 +0000

    implemetation of is_valid function

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,8 @@ def hash_password(password: str) -> bytes:
     b_password = password.encode("utf-8")
     hashed_pwd = bcrypt.hashpw(b_password, salt)
     return hashed_pwd
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """function that expects 2 arguments """
+    return bcrypt.checkpw(password, hashed_password)
