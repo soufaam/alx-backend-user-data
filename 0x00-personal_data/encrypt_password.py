392802a1af9b626ccc9fa82517388846eff8c775
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-04-13 12:43:06 +0000

    encrypt_password.py

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -4,9 +4,9 @@ import bcrypt
 import typing
 
 
-def hash_password(password: str) -> typing.ByteString:
+def hash_password(password: str) -> bytes:
     """hash password"""
     salt = bcrypt.gensalt()
-    b_password = password.encode()
+    b_password = password.encode("utf-8")
     hashed_pwd = bcrypt.hashpw(b_password, salt)
     return hashed_pwd
