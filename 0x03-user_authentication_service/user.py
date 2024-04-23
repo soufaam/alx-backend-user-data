a3d79e0c6b780866ee19884e37bf51691c3f1dd2
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-04-23 23:59:32 +0100

    create user

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -13,7 +13,7 @@ class User(Base):
     Base (_type_): _description_
     """
     __tablename__ = 'users'
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
     session_id = Column(String(250), nullable=True)
