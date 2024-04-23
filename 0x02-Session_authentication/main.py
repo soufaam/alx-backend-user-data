f162687008f451088cfe531dafc1e2c028443a42
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-04-23 22:52:01 +0100

    User model

--- 0x02-Session_authentication/main.py
+++ 0x02-Session_authentication/main.py
@@ -4,8 +4,8 @@
 import requests
 
 if __name__ == "__main__":
-    r = requests.get('http://0.0.0.0:5000/api/v1/auth_session/login')
-    if r.status_code != 404 and r.status_code != 405:
+    r = requests.get('http://0.0.0.0:5000/api/v1/users/me', cookies={'_my_session_id': "fake session ID"})
+    if r.status_code != 403:
         print("Wrong status code: {}".format(r.status_code))
         exit(1)
     print("OK", end="")
