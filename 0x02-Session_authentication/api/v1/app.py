7ab63a4162e3143652aab089aa8955efd8d9c858
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-04-23 23:07:08 +0100

    fix git problem

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -32,9 +32,7 @@ def before_request_func():
         if auth.require_auth(request.path, [
                 '/api/v1/status/',
                 '/api/v1/unauthorized/',
-                '/api/v1/forbidden/',
-                '/api/v1/auth_session/login/'
-                ]):
+                '/api/v1/forbidden/']):
             if auth.authorization_header(request) is None:
                 abort(401)
             if auth.current_user(request) is None:
--- 0x02-Session_authentication/main.py
+++ 0x02-Session_authentication/main.py
@@ -1,11 +1,26 @@
-#!/usr/bin/python3
-""" Check response
+#!/usr/bin/env python3
+""" Cookie server
 """
-import requests
+from flask import Flask, request
+from api.v1.auth.auth import Auth
+
+auth = Auth()
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def root_path():
+    """ Root path
+    """
+    return "Cookie value: {}\n".format(auth.session_cookie(request))
 
 if __name__ == "__main__":
+<<<<<<< HEAD
     r = requests.get('http://0.0.0.0:5000/api/v1/users/me', cookies={'_my_session_id': "fake session ID"})
     if r.status_code != 403:
         print("Wrong status code: {}".format(r.status_code))
         exit(1)
     print("OK", end="")
+=======
+    app.run(host="0.0.0.0", port="5000")
+>>>>>>> parent of a638005... dix bug
