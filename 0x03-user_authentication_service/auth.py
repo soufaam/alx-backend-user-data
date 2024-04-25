d2940810ecc18c8c0e8966b4f1a8728384e50a62
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-04-25 16:51:04 +0100

    update password endpoint 02

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -92,6 +92,8 @@ class Auth:
 
     def update_password(self, reset_token: str, password: str) -> None:
         """update the user’s hashed_password"""
+        if not reset_token or not password:
+            raise ValueError
         try:
             user = self._db.find_user_by(reset_token=reset_token)
             if user:
