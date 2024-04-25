43e19b7cff0b4098c5e8b823fe63f08f12ca0cf2
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-04-25 10:09:03 +0100

    Login and store session_id

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -3,7 +3,7 @@
 from flask import Flask
 from flask import jsonify
 from auth import Auth
-from flask import request
+from flask import request, abort, make_response, Response
 
 
 AUTH = Auth()
@@ -26,6 +26,19 @@ def users():
         return jsonify({"email": f"{user.email}", "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'])
+def login():
+    """login function"""
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if AUTH.valid_login(email=email, password=password):
+        response = jsonify({"email": email, "message": "logged in"})
+        response.set_cookie("session_id", AUTH.create_session(email))
+        return response
+    else:
+        abort(401)
 
 
 if __name__ == "__main__":
