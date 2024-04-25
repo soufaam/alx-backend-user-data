06c5079628768256bbf7db7062490117f12baa49
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-04-25 10:24:25 +0100

    Destroy session 01

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -68,8 +68,9 @@ class Auth:
         except Exception:
             return None
 
-    def destroy_session(self, user_id: int):
+    def destroy_session(self, user_id: int) -> None:
         """Destry session"""
         user = self._db.find_user_by(user_id=user_id)
         user.session_id = None
+        self._db._session.commit()
         return None
