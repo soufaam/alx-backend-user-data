2406b5e5dfc59ca88a832f144e5c230e2eed234a
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-04-26 01:19:07 +0100

    End-to-end integration test

--- 0x03-user_authentication_service/main.py
+++ 0x03-user_authentication_service/main.py
@@ -3,6 +3,7 @@
 Main file
 """
 import requests
+import uuid
 
 
 EMAIL = "guillaume@holberton.io"
@@ -11,39 +12,81 @@ NEW_PASSWD = "t4rt1fl3tt3"
 
 
 def register_user(email: str, password: str) -> None:
-    pass
+    """Register user"""
+    url = "http://127.0.0.1:5000/users"
+    response = requests.post(url=url, data={
+        "email": email, "password": password})
+    assert response.status_code == 200
+    assert response.json() == {
+        "email": email, "message": "user created"}
 
 
 def log_in_wrong_password(email: str, password: str) -> None:
-    pass
+    """Wrong Login user"""
+    url = "http://127.0.0.1:5000/sessions"
+    response = requests.post(url=url, data={
+        "email": email, "password": password})
+    assert response.status_code == 401
 
 
 def log_in(email: str, password: str) -> str:
-    pass
+    """Log in """
+    url = "http://127.0.0.1:5000/sessions"
+    response = requests.post(url=url, data={
+        "email": email, "password": password})
+    session_id = response.cookies.get("session_id")
+    assert response.status_code == 200
+    assert response.json() == {
+        "email": email, "message": "logged in"}
+    return session_id
 
 
 def profile_unlogged() -> None:
-    pass
+    """Log out"""
+    cookies = {'session_id': f'{uuid.uuid4().__str__()}'}
+    url = "http://127.0.0.1:5000/profile"
+    response = requests.get(url=url, cookies=cookies)
+    assert response.status_code == 403
 
 
 def profile_logged(session_id: str) -> None:
-    pass
+    """profile logged"""
+    cookies = {'session_id': session_id}
+    url = "http://127.0.0.1:5000/profile"
+    response = requests.get(url=url, cookies=cookies)
+    assert response.status_code == 200
+    assert response.json() == {"email": EMAIL}
 
 
 def log_out(session_id: str) -> None:
-    pass
+    """log out func"""
+    url = "http://127.0.0.1:5000/sessions"
+    response = requests.delete(url=url, cookies={"session_id": session_id})
+    assert response.json() == {'message': 'Bienvenue'}
+    assert response.status_code == 200
 
 
 def reset_password_token(email: str) -> str:
-    pass
+    """reset password token"""
+    url = "http://127.0.0.1:5000/reset_password"
+    response = requests.post(url=url, data={"email": email})
+    reset_token = response.json().get("reset_token")
+    assert response.status_code == 200
+    return reset_token
 
 
 def update_password(email: str, reset_token: str, new_password: str) -> None:
-    pass
+    """password update"""
+    url = "http://127.0.0.1:5000/reset_password"
+    response = requests.put(url=url, data={
+        "email": email,
+        "reset_token": reset_token,
+        "new_password": new_password})
+    assert response.status_code == 200
+    assert response.json() == {"email": email, "message": "Password updated"}
 
 
 if __name__ == "__main__":
-
     register_user(EMAIL, PASSWD)
     log_in_wrong_password(EMAIL, NEW_PASSWD)
     profile_unlogged()
