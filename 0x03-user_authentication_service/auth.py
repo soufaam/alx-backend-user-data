7af4b1c191e85c15b68bb758d4794fe10891d4ef
Soufiane Aamari <Soufianeaamari@gmail.com>
2024-04-27 00:19:02 +0100

    add UserT type

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -5,7 +5,10 @@ from db import DB
 from user import User
 from sqlalchemy.orm.exc import NoResultFound
 import uuid
-from typing import Optional
+from typing import Optional, TypeVar
+
+
+UserT = TypeVar("UserT", bound=User)
 
 
 def _hash_password(password: str) -> bytes:
@@ -58,7 +61,7 @@ class Auth:
         self._db.update_user(found_user.id, session_id=session_id)
         return session_id
 
-    def get_user_from_session_id(self, session_id: str):
+    def get_user_from_session_id(self, session_id: str) -> UserT:
         """eturns the corresponding User or None"""
         if not session_id:
             return None
